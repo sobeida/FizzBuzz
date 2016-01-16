3d4ee3686ed549db786088c11e6e18be115d68fe
sobeida <sobeidalagrange7@gmail.com>
2016-01-15 23:24:00 -0500

    JavaScript Challenge
    Numbers divisible by 3 & 5...

---
+++ app.js
@@ -0,0 +1,12 @@
+for(var i = 1; i <=100; i++) {
+	if (i % 3 == 0) {
+	console.log("Fizz");
+	}
+	if (i % 5 == 0) {
+	console.log("Buzz");
+	}
+	if (i % 15 == 0) {
+	console.log("FizzBuzz");
+	}
+	console.log(i);
+}
