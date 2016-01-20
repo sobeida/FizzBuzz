e64904c6e37c98cf56af2856d7f4d62d28030cd2
sobeida <sobeidalagrange7@gmail.com>
2016-01-19 22:51:19 -0500

    Refactor Challenge FizzBuzz app
    Function-values to be used

--- app.js
+++ app.js
@@ -10,3 +10,22 @@ for(var i = 1; i <=100; i++) {
 	}
 	console.log(i);
 }
+function myFunction(maxNumber) {
+	for(var i = 1; i <=maxNumber; i++) {
+	if (i % 3 == 0) {
+	console.log("Fizz");
+	}
+	if (i % 5 == 0) {
+	console.log("Buzz");
+	}
+	if (i % 15 == 0) {
+	console.log("FizzBuzz");
+	}
+	console.log(i);
+}
+}
+var person = prompt("Please enter a number");
+	if (person != null) {
+myFunction(person); 
+	}
+
